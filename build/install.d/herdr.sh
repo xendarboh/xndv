a258d80ae376a6b64b31ca3f9f3c6bb4308d5077
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2026-08-06 16:00:02 -0700

    chore(herdr): ensure custom config when installed on host

--- build/install.d/herdr.sh
+++ build/install.d/herdr.sh
@@ -2,5 +2,11 @@
 set -euo pipefail
 
 curl -fsSL https://herdr.dev/install.sh | sh
+
 export PATH="$HOME/.local/bin:$PATH"
+
 herdr plugin install lmilojevicc/herdr-splits.nvim --yes
+
+# ensure custom config when this script is run on host
+# this is redundant for xndv, which gets the config via stow
+ln -sfv ~/src/xndv/conf/.config/herdr/config.toml ~/.config/herdr/config.toml
